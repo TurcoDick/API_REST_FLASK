f8f630d36f9e655cff50b738aca8087ca4fb7718
Alison Lucio Santos <aluciosa@everis.com>
2020-01-09 16:39:21 -0300

    get, post funcionando

--- resources/hotel.py
+++ resources/hotel.py
@@ -1,4 +1,4 @@
-from flask_restful import Resource
+from flask_restful import Resource, reqparse
 
 hoteis = [
     {
@@ -43,8 +43,46 @@ class Hoteis(Resource):
     def get(self):
         return {'hoteis' : hoteis}
 
-    def put(self):
-       pass
 
-    def delete(self):
+class Hotel(Resource):
+
+    def find_hotel(hotel_id):
+        for hotel in hoteis:
+            if hotel['hotel_id'] == hotel_id:
+                return hotel
+
+    def get(self, hotel_id):
+        hotel = Hotel.find_hotel(hotel_id)
+        if hotel:
+            return hotel
+        return {'message' : 'Hotel not found. '}, 404
+
+    def post(self, hotel_id):
+        argumentos = reqparse.RequestParser()
+        argumentos.add_argument('nome')
+        argumentos.add_argument('estrelas')
+        argumentos.add_argument('diaria')
+        argumentos.add_argument('cidade')
+
+        dados = argumentos.parse_args()
+
+        novo_hotel = {
+            'hotel_id' : hotel_id,
+            'nome' : dados['nome'],
+            'estrelas': dados['estrelas'],
+            'diaria' : dados['diaria'],
+            'cidade' : dados['cidade']
+        }
+
+        hoteis.append(novo_hotel)
+        return novo_hotel, 200
+
+    def put(self, hotel_id):
+        hotel = Hotel.find_hotel(hotel_id)
+        if hotel:
+            pass
+        else:
+            Hotel.post(hotel_id)
+
+    def delete(self, hotel_id):
         pass
